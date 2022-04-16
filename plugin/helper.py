301bcf55dba338f0ee8f7cc79589477b15b0d481
Daniel Duckworth <18698360+danielduckworth@users.noreply.github.com>
2022-04-17 06:07:36 +1000

    Fix RPC format

--- plugin/helper.py
+++ plugin/helper.py
@@ -5,15 +5,17 @@ def process_result(result):
 
     if object_type == "page":
         title = result['properties']['Name']['title'][0]['plain_text']
-    else:
+    elif object_type == "database":
         title = result['title'][0]['plain_text']
+    else:
+        title = None
 
     return {
-        "Title": title,
-        "SubTtitle": f"Notion {object_type}",
-        "IcoPath": "Images\\notion-black.svg",
-        "JsonRPCAction":
-        {
-            "method": "open_url", 
-            "parameters": result['url']
-            }}
+                "Title": title,
+                "SubTitle": f"Notion {object_type}",
+                "IcoPath": "Images/notion.png",
+                "JsonRPCAction": {
+                    "method": "open_url",
+                    "parameters": [result['url']]
+                }
+            }

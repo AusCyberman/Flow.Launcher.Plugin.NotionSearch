73e4dc43f3831e0218e9552a846d83e0b1349ac2
Daniel Duckworth <18698360+danielduckworth@users.noreply.github.com>
2022-04-19 11:55:14 +1000

    Add some error handling

--- plugin/notion_search.py
+++ plugin/notion_search.py
@@ -12,17 +12,51 @@ from helper import process_result
 class NotionSearch(FlowLauncher):
 
     def query(self, query):
+
         settings = self.rpc_request.get("settings", {})
 
-        # Get user defined notion token from plugin settings in the flow launcher application
-        NOTION_TOKEN = settings.get("notion_token")
-        # Initialize the client with the token
-        notion = Client(auth=NOTION_TOKEN)
+        # Check if the user has set an integration token
+        try:
+            NOTION_TOKEN = settings.get("notion_token")
+            if not NOTION_TOKEN:
+                raise Exception("No token found")
+        except Exception as e:
+            logging.error(e)
 
-        # Query notion API
-        api_results = notion.search(query=query).get("results")
+            return [{
+                "Title": "Notion integration token not found.",
+                "SubTitle": "Please set the token in the settings of the plugin in the flow launcher application.",
+                "IcoPath": "Images/notion.png",
+                "JsonRPCAction": {
+                    "method": "Flow.Launcher.OpenSettingDialog",
+                    "parameters": []
+                }
+            }]
 
-        # Process the results with the helper function
+        try:
+            # Initialize the client with the token
+            notion = Client(auth=NOTION_TOKEN)
+
+            # Query notion API
+            api_results = notion.search(query=query).get("results")
+
+            # If the request has errors, return an error message
+            # TODO: Check if the error is a 404 or a 401 so that empty results can be returned
+            if not api_results:
+                raise Exception("No results returned")
+        except Exception as e:
+            logging.error(e)
+
+            return [{
+                "Title": "Notion API returned an error.",
+                "SubTitle": "Check that your integration token is valid in the settings of the plugin in the flow launcher application.",
+                "IcoPath": "Images/notion.png",
+                "JsonRPCAction": {
+                    "method": "Flow.Launcher.OpenSettingDialog",
+                    "parameters": []
+                }
+            }]
+
         return [process_result(result) for result in api_results]
 
     def context_menu(self, data):

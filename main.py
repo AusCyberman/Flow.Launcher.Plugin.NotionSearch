723324edf0d26be3f372164be12a4ac9274d1556
Daniel Duckworth <18698360+danielduckworth@users.noreply.github.com>
2022-05-03 09:44:25 +1000

    Formatting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+"""Main module."""
 
-import sys,os
+import sys
+import os
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
